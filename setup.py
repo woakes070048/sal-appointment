e0ed0b364a215ab61ba553dd65b66ef1cf9f906e
taherkhalil <taherkhalil52@gmail.com>
2019-01-05 20:44:18 +0300

    revert back to old setup version

--- setup.py
+++ setup.py
@@ -1,25 +1,26 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
+from pip.req import parse_requirements
 import re, ast
 
-with open('requirements.txt') as f:
-	install_requires = f.read().strip().split('\n')
-
-# get version from __version__ variable in recipe_master/__init__.py
+# get version from __version__ variable in appointment/__init__.py
 _version_re = re.compile(r'__version__\s+=\s+(.*)')
 
-with open('recipe_master/__init__.py', 'rb') as f:
-	version = str(ast.literal_eval(_version_re.search(
-		f.read().decode('utf-8')).group(1)))
+with open('appointment/__init__.py', 'rb') as f:
+    version = str(ast.literal_eval(_version_re.search(
+        f.read().decode('utf-8')).group(1)))
+
+requirements = parse_requirements("requirements.txt", session="")
 
 setup(
 	name='appointment',
 	version=version,
-	description='Appointment creation',
-	author='Taher',
+	description='Appointment manager with schedule calendar and all in one interface',
+	author='Taher khalil',
 	author_email='taherkhalil52@gmail.com',
 	packages=find_packages(),
 	zip_safe=False,
 	include_package_data=True,
-	install_requires=install_requires
+	install_requires=[str(ir.req) for ir in requirements],
+	dependency_links=[str(ir._link) for ir in requirements if ir._link]
 )
